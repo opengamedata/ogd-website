1c1fccea7914649aebd2262c2cfad1047a874262
Luke Swanson <lwswanson2@wisc.edu>
2026-01-01 13:07:23 -0600

    Add next couple of classes that are ready to go.

---
+++ src/models/GameUsage.py
@@ -0,0 +1,90 @@
+import json
+from typing import Any, List, Optional
+
+class MonthSessions:
+    def __init__(self, year:int, month:int, total_sessions:int):
+        self._year = year
+        self._month = month
+        self._total_sessions = total_sessions
+
+    @property
+    def Year(self) -> int:
+        return self._year
+    @property
+    def Month(self) -> int:
+        return self._month
+    @property
+    def TotalSessions(self) -> int:
+        return self._total_sessions
+
+class GameUsage:
+    def __init__(self, game_id:Optional[str], months:List[MonthSessions]):
+        self._game_id = game_id
+        self._months = months
+
+    @staticmethod
+    def fromObj(obj):
+        return GameUsage(
+            game_id  = obj.get("game_id"),
+            months = obj.get("sessions", [])
+        )
+
+    @staticmethod
+    def fromJson(raw_json):
+        obj = json.loads(raw_json)
+        return GameUsage.fromObj(obj)
+
+    # get methods
+    @property
+    def ID(self):
+        return self._game_id
+    @property
+    def Months(self) -> List[MonthSessions]:
+        return self._months
+    @property
+    def Sessions(self) -> List[MonthSessions]:
+        """Alias for Months property
+
+        :return: _description_
+        :rtype: List[MonthSessions]
+        """
+        return self.Months
+    @property
+    def LatestMonthlySessions(self):
+        return self.Months[-1].TotalSessions if len(self.Months) > 0 else 0
+
+    @property
+    def AverageMonthlySessionsOverMostRecentActiveYear(self) -> int:
+        """Find the most-recent month that had sessions
+        
+        Sum up the number of sessions in that month and previous months, going back as many as 12 months
+        Return the average number of sessions per month over that range
+
+        :return: _description_
+        :rtype: _type_
+        """
+        ret_val = 0
+
+        months_counted = 0
+        sum_sessions   = 0
+
+        # Loop through months from most recent to oldest
+        for month in reversed(self._months):
+            # If this month has sessions
+            if month.TotalSessions > 0:
+                # Add it to our sum
+                sum_sessions += month.TotalSessions
+            # If we've found a month with sessions either this iteration or a previous iteration
+            if sum_sessions > 0:
+                # This month counts towards our 12 whether or not it had sessions
+                months_counted += 1
+            # Once we have 12 months of data we can quit
+            if months_counted == 12:
+                break
+
+        # If we counted at least one month
+        if months_counted > 0:
+            # Return an integer average
+            ret_val = (int)(sum_sessions / months_counted)
+
+        return ret_val
