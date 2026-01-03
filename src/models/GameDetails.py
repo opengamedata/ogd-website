512cd7f2076c7a4d61e944304098c32cd00674f5
Luke Swanson <lwswanson2@wisc.edu>
2026-01-02 21:13:12 -0600

    Make a few params optional for GameDetails, and rename FromArray -> FromDict, since that's what we're dealing with in python terms.

--- src/models/GameDetails.py
+++ src/models/GameDetails.py
@@ -1,21 +1,23 @@
 import html
-from typing import List
+from typing import List, Optional
+
+from ogd.common.utils.typing import Map
 
 from models.Publication import Publication
 
 class GameDetails:
     def __init__(self, game_id:str,  name:str,      description:str,
-                 thumbnail_path:str, play_link:str, source_link:str,
-                 developer_name:str, developer_link:str,
+                 thumbnail_path:Optional[str], play_link:Optional[str], source_link:Optional[str],
+                 developer_name:Optional[str], developer_link:Optional[str],
                  publications:List[Publication]):
         self._game_id          : str               = game_id
         self._game_name        : str               = name
         self._game_description : str               = description
-        self._thumbnail_path   : str               = thumbnail_path
-        self._play_link        : str               = play_link
-        self._source_link      : str               = source_link
-        self._developer_name   : str               = developer_name
-        self._developer_link   : str               = developer_link
+        self._thumbnail_path   : Optional[str]     = thumbnail_path
+        self._play_link        : Optional[str]     = play_link
+        self._source_link      : Optional[str]     = source_link
+        self._developer_name   : Optional[str]     = developer_name
+        self._developer_link   : Optional[str]     = developer_link
         self._publications     : List[Publication] = publications
 
     @staticmethod
@@ -23,10 +25,10 @@ class GameDetails:
         # game object returned from api
         data = json.loads(json)
 
-        return GameDetails.FromArray(game_id, data)
+        return GameDetails.FromDict(game_id, data)
 
     @staticmethod
-    def FromArray(game_id, data) -> "GameDetails":
+    def FromDict(game_id:str, data:Map) -> "GameDetails":
         # build the array for publications
         publications = []
         for value in data.get('studies', []):
@@ -34,13 +36,13 @@ class GameDetails:
 
         return GameDetails(
             game_id=game_id,
-            name=data.get('game_name'),
-            description=data.get('game_description'),
+            name=data.get('game_name', "UNRECOGNIZED GAME"),
+            description=data.get('game_description', "N/A"),
             thumbnail_path=data.get('thumbnail_path'),
             play_link=data.get('play_link'),
             source_link=data.get('source_link'),
-            developer_name=data.get('developers')[0].get('name'),
-            developer_link=data.get('developers')[0].get('link'),
+            developer_name=data.get('developers', [])[0].get('name'),
+            developer_link=data.get('developers', [])[0].get('link'),
             publications=publications
         )
 
@@ -67,36 +69,36 @@ class GameDetails:
         self._game_description = description
 
     @property
-    def PlayLink(self, html_safe:bool=True) -> str:
-        return html.escape(self._play_link, quote=True) if html_safe else self._play_link
+    def PlayLink(self, html_safe:bool=True) -> Optional[str]:
+        return html.escape(self._play_link, quote=True) if html_safe and self._play_link else self._play_link
     @PlayLink.setter
     def PlayLink(self, link:str):
         self._play_link = link
 
     @property
-    def SourceLink(self, html_safe:bool=True) -> str:
-        return html.escape(self._source_link, quote=True) if html_safe else self._source_link
+    def SourceLink(self, html_safe:bool=True) -> Optional[str]:
+        return html.escape(self._source_link, quote=True) if html_safe and self._source_link else self._source_link
     @SourceLink.setter
     def SourceLink(self, link:str):
         self._source_link = link
 
     @property
-    def ThumbPath(self, html_safe:bool=True) -> str:
-        return html.escape(self._thumbnail_path, quote=True) if html_safe else self._thumbnail_path
+    def ThumbPath(self, html_safe:bool=True) -> Optional[str]:
+        return html.escape(self._thumbnail_path, quote=True) if html_safe and self._thumbnail_path else self._thumbnail_path
     @ThumbPath.setter
     def ThumbPath(self, path:str):
         self._thumbnail_path = path
 
     @property
-    def DeveloperName(self, html_safe:bool=True) -> str:
-        return html.escape(self._developer_name, quote=True) if html_safe else self._developer_name
+    def DeveloperName(self, html_safe:bool=True) -> Optional[str]:
+        return html.escape(self._developer_name, quote=True) if html_safe and self._developer_name else self._developer_name
     @DeveloperName.setter
     def DeveloperName(self, name:str):
         self._developer_name = name
 
     @property
-    def DeveloperLink(self, html_safe:bool=True) -> str:
-        return html.escape(self._developer_link, quote=True) if html_safe else self._developer_link
+    def DeveloperLink(self, html_safe:bool=True) -> Optional[str]:
+        return html.escape(self._developer_link, quote=True) if html_safe and self._developer_link else self._developer_link
     @DeveloperLink.setter
     def DeveloperLink(self, link:str):
         self._developer_link = link
