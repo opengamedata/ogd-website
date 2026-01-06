9d4595fe696cd4475f1ed0168c24e56aed76c391
Luke Swanson <lwswanson2@wisc.edu>
2026-01-05 21:27:14 -0600

    Make GameCard carry all data from the Card structure, was stupid to have these separate.

--- src/models/GameCard.py
+++ src/models/GameCard.py
@@ -1,17 +1,22 @@
+from typing import Optional
+
 from models.GameDetails import GameDetails
 from models.GameUsage import GameUsage
+from includes.utils import num_in_kilo
 
 class GameCard:
     """The data used by a GameCard
-    
-    TODO: kick this stupid structure to the curb, pass the other two objects separately. We're not saving much with this.
     """
-    def __init__(self, game:GameDetails, game_usage:GameUsage):
+    def __init__(self, game:GameDetails, game_usage:Optional[GameUsage]=None):
         self._game = game
         self._game_usage = game_usage
+        self._game_link = f"gamedata.html?game={game.ID}"
+        self._monthly_sessions = num_in_kilo(game_usage.AverageMonthlySessions()) if game_usage else "0"
 
     # get methods
-    def getGame(self) -> GameDetails:
+    @property
+    def Game(self) -> GameDetails:
         return self._game
-    def getGameUsage(self) -> GameUsage:
+    @property
+    def GameUsage(self) -> Optional[GameUsage]:
         return self._game_usage
