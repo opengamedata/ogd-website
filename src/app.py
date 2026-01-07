3ac9b3cd1b520738da82ddbe2259d54a551eb028
Luke Swanson <lwswanson2@wisc.edu>
2026-01-06 20:17:52 -0600

    Mark assets as the folder for static resources.

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ from models.GameFileInfo import GameFileInfo
 from models.GameUsage import GameUsage
 from models.PipelineElement import PipelineElement
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="assets")
 
 @app.route("/")
 @app.route("/index.html")
