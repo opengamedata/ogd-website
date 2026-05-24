d77427626475aabd13d3af15f803f33d9b9d37b1
Luke Swanson <lwswanson2@wisc.edu>
2026-05-24 16:16:13 -0500

    Comment out old API call functions.

--- src/includes/services.py
+++ src/includes/services.py
@@ -17,7 +17,6 @@ from config.AppConfig import AppConfig
 # from models.APIResponse import APIResponse
 from models.GameDetails import GameDetails
 from models.GameFileInfo import GameFileInfo
-from models.GameUsage import GameUsage
 
 def getGameList() -> Dict[str, GameDetails]:
     """ Get Games from game_list
@@ -60,103 +59,103 @@ def getGameDetails(game_id:str) -> Optional[GameDetails]:
 #         err_str = f"getGameUsageByMonth request, for game_id={game_id} with year={year} and month={month}, got no response object!"
 #         current_app.logger.error(err_str)
 
-def getGameUsageByMonth(game_id:str, year:Optional[int] = None, month : Optional[int] = None) -> Optional[GameUsage]:
-    """ Get game usage from API
-    * <param> string game_id
-    * <param> string year - optional
-    * <param> string month - optional
-    """
-    ret_val = None
+# def getGameUsageByMonth(game_id:str, year:Optional[int] = None, month : Optional[int] = None) -> Optional[GameUsage]:
+#     """ Get game usage from API
+#     * <param> string game_id
+#     * <param> string year - optional
+#     * <param> string month - optional
+#     """
+#     ret_val = None
 
-    params = {
-        'game_id': game_id,
-        'year'   : year,
-        'month'  : month
-    }
+#     params = {
+#         'game_id': game_id,
+#         'year'   : year,
+#         'month'  : month
+#     }
 
-    # 1. Make request to API
-    usage_url = urljoin(base=AppConfig.APP_CONFIG.get('WEBSITE_API_URL_BASE', "http://localhost:5000"), url="getGameUsageByMonth")
-    api_response = APIRequest(url=usage_url, request_type=RESTType.GET, params=params).Execute(current_app.logger)
+#     # 1. Make request to API
+#     usage_url = urljoin(base=AppConfig.APP_CONFIG.get('WEBSITE_API_URL_BASE', "http://localhost:5000"), url="getGameUsageByMonth")
+#     api_response = APIRequest(url=usage_url, request_type=RESTType.GET, params=params).Execute(current_app.logger)
 
-    # 2. Convert response to a GameUsage object
-    if api_response:
-        if api_response.Status == ResponseStatus.OK:
-            if api_response.Value:
-                ret_val = GameUsage.FromDict(api_response.Value)
-            else:
-                err_str = f"getGameUsageByMonth request, for game_id={game_id} with year={year} and month={month}, had empty value element!:\nResponse message: {api_response.Message}"
-                current_app.logger.error(err_str)
-        else:
-            err_str = f"getGameUsageByMonth request, for game_id={game_id} with year={year} and month={month}, was unsuccessful:\n{api_response.Message}"
-            current_app.logger.error(err_str)
-    else:
-        err_str = f"getGameUsageByMonth request, for game_id={game_id} with year={year} and month={month}, got no response object!"
-        current_app.logger.error(err_str)
+#     # 2. Convert response to a GameUsage object
+#     if api_response:
+#         if api_response.Status == ResponseStatus.OK:
+#             if api_response.Value:
+#                 ret_val = GameUsage.FromDict(api_response.Value)
+#             else:
+#                 err_str = f"getGameUsageByMonth request, for game_id={game_id} with year={year} and month={month}, had empty value element!:\nResponse message: {api_response.Message}"
+#                 current_app.logger.error(err_str)
+#         else:
+#             err_str = f"getGameUsageByMonth request, for game_id={game_id} with year={year} and month={month}, was unsuccessful:\n{api_response.Message}"
+#             current_app.logger.error(err_str)
+#     else:
+#         err_str = f"getGameUsageByMonth request, for game_id={game_id} with year={year} and month={month}, got no response object!"
+#         current_app.logger.error(err_str)
 
-    return ret_val
+#     return ret_val
 
-def getGameFileInfoByMonth(game_id:str, year:Optional[int]=None, month:Optional[int]=None) -> Optional[GameFileInfo]:
-    """ Get game file info from API
-    * <param> string game_id
-    * <param> string year --optional
-    * <param> string month --optional
-    """
-    ret_val = None
+# def getGameFileInfoByMonth(game_id:str, year:Optional[int]=None, month:Optional[int]=None) -> Optional[GameFileInfo]:
+#     """ Get game file info from API
+#     * <param> string game_id
+#     * <param> string year --optional
+#     * <param> string month --optional
+#     """
+#     ret_val = None
 
-    params = {
-        'game_id' : game_id,
-        'year' : year,
-        'month' : month
-    }
+#     params = {
+#         'game_id' : game_id,
+#         'year' : year,
+#         'month' : month
+#     }
     
-    # 1. Make request to API via cURL
-    info_url = urljoin(base=AppConfig.APP_CONFIG.get('WEBSITE_API_URL_BASE', "http://localhost:5000"), url="getGameFileInfoByMonth")
-    api_response : APIResponse = APIRequest(url=info_url, request_type=RESTType.GET, params=params, timeout=5).Execute(current_app.logger)
+#     # 1. Make request to API via cURL
+#     info_url = urljoin(base=AppConfig.APP_CONFIG.get('WEBSITE_API_URL_BASE', "http://localhost:5000"), url="getGameFileInfoByMonth")
+#     api_response : APIResponse = APIRequest(url=info_url, request_type=RESTType.GET, params=params, timeout=5).Execute(current_app.logger)
 
-    # 2. Convert response to a GameFileInfo object
-    if api_response:
-        if api_response.Status == ResponseStatus.OK:
-            if api_response.Value:
-                ret_val = GameFileInfo.FromDict(api_response.Value, game_id=game_id)
-            else:
-                err_str = f"getGameFileInfoByMonth request, for game_id={game_id} with year={year} and month={month}, had empty value element!\nResponse message: {api_response.Message}"
-                current_app.logger.error(err_str)
-        else:
-            err_str = f"getGameFileInfoByMonth request, for game_id={game_id} with year={year} and month={month}, was unsuccessful:\n{api_response.Message}"
-            current_app.logger.error(err_str)
-    else:
-        err_str = f"getGameFileInfoByMonth request, for game_id={game_id} with year={year} and month={month}, got no response object!"
-        current_app.logger.error(err_str)
+#     # 2. Convert response to a GameFileInfo object
+#     if api_response:
+#         if api_response.Status == ResponseStatus.OK:
+#             if api_response.Value:
+#                 ret_val = GameFileInfo.FromDict(api_response.Value, game_id=game_id)
+#             else:
+#                 err_str = f"getGameFileInfoByMonth request, for game_id={game_id} with year={year} and month={month}, had empty value element!\nResponse message: {api_response.Message}"
+#                 current_app.logger.error(err_str)
+#         else:
+#             err_str = f"getGameFileInfoByMonth request, for game_id={game_id} with year={year} and month={month}, was unsuccessful:\n{api_response.Message}"
+#             current_app.logger.error(err_str)
+#     else:
+#         err_str = f"getGameFileInfoByMonth request, for game_id={game_id} with year={year} and month={month}, got no response object!"
+#         current_app.logger.error(err_str)
 
-    return ret_val
+#     return ret_val
 
-def getGameUsage(game_id:str) -> Optional[GameUsage]:
-    """ Get game usage from API
-    * <param> string game_id
-    """
-    ret_val = None
+# def getGameUsage(game_id:str) -> Optional[GameUsage]:
+#     """ Get game usage from API
+#     * <param> string game_id
+#     """
+#     ret_val = None
 
-    params = {
-        'game_id' : game_id
-    }
+#     params = {
+#         'game_id' : game_id
+#     }
     
-    # 1. Make request to API via cURL
-    usage_url = urljoin(base=AppConfig.APP_CONFIG['WEBSITE_API_URL_BASE'], url="getMonthlyGameUsage")
-    api_response = APIRequest(url=usage_url, request_type=RESTType.GET, params=params).Execute(current_app.logger)
+#     # 1. Make request to API via cURL
+#     usage_url = urljoin(base=AppConfig.APP_CONFIG['WEBSITE_API_URL_BASE'], url="getMonthlyGameUsage")
+#     api_response = APIRequest(url=usage_url, request_type=RESTType.GET, params=params).Execute(current_app.logger)
 
-    # 2. Convert response to a GameUsage object
-    if api_response:
-        if api_response.Status == ResponseStatus.OK:
-            if api_response.Value:
-                ret_val = GameUsage.FromDict(api_response.Value)
-            else:
-                err_str = f"getGameUsage request, for game_id={game_id}, had empty value element!\nResponse message: {api_response.Message}"
-                current_app.logger.error(err_str)
-        else:
-            err_str = f"getGameUsage request, for game_id={game_id}, was unsuccessful:\n{api_response.Message}"
-            current_app.logger.error(err_str)
-    else:
-        err_str = f"getGameUsage request, for game_id={game_id}, got no response object!"
-        current_app.logger.error(err_str)
+#     # 2. Convert response to a GameUsage object
+#     if api_response:
+#         if api_response.Status == ResponseStatus.OK:
+#             if api_response.Value:
+#                 ret_val = GameUsage.FromDict(api_response.Value)
+#             else:
+#                 err_str = f"getGameUsage request, for game_id={game_id}, had empty value element!\nResponse message: {api_response.Message}"
+#                 current_app.logger.error(err_str)
+#         else:
+#             err_str = f"getGameUsage request, for game_id={game_id}, was unsuccessful:\n{api_response.Message}"
+#             current_app.logger.error(err_str)
+#     else:
+#         err_str = f"getGameUsage request, for game_id={game_id}, got no response object!"
+#         current_app.logger.error(err_str)
 
-    return ret_val
+#     return ret_val
